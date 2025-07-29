53565a71a92c0497d99f5c25f5d53ad27f78906f
zhengxiaolang <haifeng3099@126.com>
2025-07-29 14:37:37 +0800

    add ios project

---
+++ appleAI/appleAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appleAI
+//
+//  Created by Brown Zheng on 2025/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
